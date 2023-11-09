b73c7a6c62a8c602e4c08a8fcc38315cec56e71b
Gaurav Jangam <sam.herambk@gmail.com>
2023-11-09 23:22:49 +0530

    all if else programs 09 Nov 2023

---
+++ differenceOfNumbers.js
@@ -0,0 +1,13 @@
+    let value1 = 90;
+    let value2 = 87;
+    const diff1 = Math.abs(value1 - 100);
+    const diff2 = Math.abs(value2 - 100);
+  
+    if (diff1 < diff2) {
+      console.log(value1) ;
+    } else {
+      console.log(value2) ;
+    }
+  
+
+  
