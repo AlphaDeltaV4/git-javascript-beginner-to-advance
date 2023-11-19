e4d33fc8aa1b687ef41d6d84f1e61b3743f5c069
Gaurav Jangam <sam.herambk@gmail.com>
2023-11-19 20:28:58 +0530

    center and right stars patterns

--- rightStar.js
+++ rightStar.js
@@ -1,12 +1,29 @@
-let row = "";
+// let row = "";
 
-for (let i = 4; i <= 4; i++) { //rows
-  row += "  ";
-  for(let j=4; j<=4; j++){ //columns
-    row += "*";
+// for (let i = 4; i <= 4; i++) { //rows
+//   row += "  ";
+//   for(let j=4; j<=4; j++){ //columns
+//     row += "*";
 
-  // console.log(row);
+//   // console.log(row);
+// }
+// }
+
+// console.log(row) ;
+
+const n = 5;
+
+for (let i = 1; i <= n; i++) {
+    let space = '';
+    for (let j = 1; j <= n - i; j++) {
+        space += ' ';
+    }
+
+    let asterisk = '';
+    for (let k = 1; k <= i; k++) {
+        asterisk += '*';
+    }
+
+    console.log(space + asterisk);
 }
-}
 
-console.log(row) ;
