efb440951c316df1140429bd952ab616382c38e3
Gaurav Jangam <sam.herambk@gmail.com>
2023-11-19 20:06:32 +0530

    factorial program

--- factorialNumber.js
+++ factorialNumber.js
@@ -1,21 +1,11 @@
-let a=5;
-let factorial;
-if (a==0){
-    factorial = 1;
+let n = 5;
+let factorial = 1;
+
+if (n === 0 || n === 1) {
+    console.log(`Factorial of ${n} is: 1`);
 } else {
-// console.log(Math.abs(90-100));
-for(i=1; i<=a; i++){
-    // console.log("a->",a);
-    // let mul = a-i;
-    // console.log("mul", mul);
-    // console.log("the_final_ans", mul*5);
-    // console.log(a*mul);
-    // console.log(a*mul);
-    console.log("a->", a);
-    console.log("i->", i);
-    console.log(a*(5-1)*(5-2)*(5-3)*(5-4));
-    console.log("Factorial>>",a*(a-i));
-    let fact = a * (a-i);   
-    // console.log(fact);
-}
+    for (let i = 2; i <= n; i++) {
+        factorial = factorial * i;
+    }
+    console.log(`Factorial of ${n} is: ${factorial}`);
 }
